80d6f1c24755034c544f3d5d67df1519f8351d88
atong27 <ayrtonilagan@gmail.com>
2020-10-25 23:29:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/mwarning/openwrt-examples
+cp openwrt-examples/example1 package/autoip
